b6f9c2642d514ded7eb0aac239fdabd337a43f04
Lina Uribe <lina.uribem@udea.edu.co>
2020-09-21 22:47:32 -0500

    Pom.xml configuration and docker-compose.yml file

---
+++ src/main/java/co/edu/udea/controlDeGastosbackend/ControlDeGastosBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.controlDeGastosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeGastosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeGastosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udea/controlDeGastosbackend/ControlDeGastosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.controlDeGastosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeGastosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
